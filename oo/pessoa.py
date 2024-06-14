e143c35874b8914b6f553cb39a7ebc2a357d8402
Ricardo Moreno <ricardo.moreno@electrolux.com>
2024-06-14 22:43:10 +0200

    Diferença entre atributos de classe e atributos de instancia

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,6 +1,12 @@
 class Pessoa:
+    olhos = 2 # Atributo de classe. 
+    #Atributo de classe eh comum a todas as instancias, mas nao eh mostrado por __dict__ por nao ser um atributo de instancia.
+    #Ao acessar o atributo olhos em uma instancia, como ele nao exite o python procura nos atributos de classe.
+    #Caso nao ache nos tributos de classe, um erro sera gerado.
+    #Todas instancias de Pessoa terao 2 olhos por default na sua criaçao e como eh comum, usa menos memoria.
+
     def __init__(self, *filhos, nome=None, idade=None):# *filhos: *argv representa um paramentro que pode receber 1 ou mais valores
-        self.nome = nome
+        self.nome = nome # atributo de instancia( consome memoria para cada nova instancia de pessoa)
         self.idade = idade
         self.filhos = list(filhos)
 
@@ -36,3 +42,7 @@ if __name__ == '__main__':
 
     for pessoa in pessoas:
         print(pessoa.__dict__)# Mostra os atributos novamente apos as modificaçoes dinamicas   
+
+    for pessoa in pessoas:
+        print(pessoa.olhos, id(pessoa.olhos), pessoa.nome, id(pessoa.nome))# Mostra os atributos olhos de todas as pessoas e seu id comprovando que sao iguais
+        
